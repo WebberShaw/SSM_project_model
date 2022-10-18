98291dd322c9bd2fd4105cb996d5532eaface638
WebberShaw <wenboxiao@stu.edu.cn>
2022-10-18 19:33:39 +0800

    Add Dao Demo

--- src/main/java/edu/hunau/config/SpringConfig.java
+++ src/main/java/edu/hunau/config/SpringConfig.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan({"edu.hunau.service", "edu.hunau.model","edu.hunau.dao","edu.hunau.utils"})
 @PropertySource({"classpath:jdbc.properties"})
 @Import({JdbcConfig.class, MybatisConfig.class})
+@EnableTransactionManagement
 public class SpringConfig {
 }
---
+++ src/main/java/edu/hunau/dao/TestDao.java
@@ -0,0 +1,9 @@
+package edu.hunau.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Repository
+public interface TestDao {
+}
