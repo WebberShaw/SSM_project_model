078a0bae5de0e058aa494bd5e35d6b30e2a0a74a
WebberShaw <wenboxiao@stu.edu.cn>
2022-10-23 20:51:09 +0800

    Interceptor added!

--- src/main/java/edu/hunau/config/SpringConfig.java
+++ src/main/java/edu/hunau/config/SpringConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration
 @ComponentScan({"edu.hunau.service", "edu.hunau.model","edu.hunau.dao","edu.hunau.utils"})
 @PropertySource({"classpath:jdbc.properties"})
 @Import({JdbcConfig.class, MybatisConfig.class})
-@EnableTransactionManagement
+@EnableTransactionManagement //Add @Transactional before Dao class
 public class SpringConfig {
 }
--- src/main/java/edu/hunau/config/SpringMvcConfig.java
+++ src/main/java/edu/hunau/config/SpringMvcConfig.java
@@ -1,11 +1,35 @@
 package edu.hunau.config;
 
+import edu.hunau.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan("edu.hunau.controller")
+@ComponentScan({"edu.hunau.controller","edu.hunau.interceptor"})
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    ProjectInterceptor projectInterceptor;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+
+        /**
+         * Used to release static resources
+         */
+//        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+//        registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(projectInterceptor).addPathPatterns("/PathYouWantToIntercept","/PathYouWantToIntercept/*");
+    }
 }
